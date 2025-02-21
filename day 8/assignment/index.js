394702aa17d6c6033d35943b9bdc4505539e3b3e
sima chowdhury <simachowdhurycse@gmail.com>
2025-02-21 14:03:43 +0530

    sima

---
+++ day 8/assignment/index.js
@@ -0,0 +1,45 @@
+
+document.getElementById("body").style.display="flex";
+document.getElementById("body").style.justifyContent="center";
+document.getElementById("body").style.alignItems="center";
+document.getElementById("body").style.height="100vh";
+document.getElementById("body").style.fontFamily="sans-serif";
+
+document.getElementById("h1").style.textAlign="center";
+document.getElementById("h1").style.color="#fff";
+document.getElementById("h1").style.textShadow="2px 2px 10px rgba(0, 0, 0, 0.7)";
+
+
+document.getElementById("main").style.width="400px";
+document.getElementById("main").style.padding="25px";
+document.getElementById("main").style.borderRadius="20px";
+document.getElementById("main").style.backgroundColor="rgba(239, 241, 241, 0.85)";
+document.getElementById("main").style.display="grid";
+document.getElementById("main").style.gridTemplateColumns="repeat(4,1fr)";
+document.getElementById("main").style.gap="12px";
+
+
+document.getElementById("keyinput").style.display="flex";
+document.getElementById("keyinput").style.gridColumn="span 4";
+document.getElementById("keyinput").style.margin="15px 0";
+
+
+document.getElementById("text").style.width="100%";
+document.getElementById("text").style.padding="25px";
+document.getElementById("text").style.border="none";
+document.getElementById("text").style.backgroundColor="#aacbda";
+document.getElementById("text").style.color="#00eaff";
+document.getElementById("text").style.fontSize="28px";
+document.getElementById("text").style.textAlign="right";
+document.getElementById("text").style.outline="none";
+
+
+// document.getElementById("num").style.display="flex";
+// document.getElementById("num").style.justifyContent="center";
+// document.getElementById("num").style.alignItems="center";
+// document.getElementById("num").style.height="65px";
+// document.getElementById("num").style.borderEndEndRadius="12px";
+// document.getElementById("num").style.color="fff";
+// document.getElementById("num").style.backgroundColor="linear-gradient(145deg, #333, #444)";
+// document.getElementById("num").style.fontWeight="bold";
+// document.getElementById("num").style.fontSize="24px";
---
+++ day 8/practice/index.js
@@ -0,0 +1,6 @@
+document.getElementById("container").style.backgroundColor="blue";
+document.getElementById("container").style.height="200px";
+document.getElementById("container").style.width="200px";
+document.getElementById("container").style.border="2px solid black";
+document.getElementById("container").style.height="200px";
+
