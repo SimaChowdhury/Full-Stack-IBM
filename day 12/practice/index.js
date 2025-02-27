49a558a3ee942ad45f00ab4ccaf1d0f01ea76960
sima chowdhury <simachowdhurycse@gmail.com>
2025-02-27 20:02:02 +0530

    sima

---
+++ day 12/practice/index.js
@@ -0,0 +1,21 @@
+// const person = {
+//     firstName: "Virat",
+//     lastName: "kohli",
+//     age: 50,
+//     eyeColor: "blue"
+//   };
+//   console.log(person)
+//   console.log(person.firstName)
+
+// let x = myFunction(24, 3);
+// function myFunction(a, b) {
+//   return a * b;
+// }
+// console.log(x)
+
+function toCelsius(fahrenheit) {
+    return (5/9) * (fahrenheit-32);
+  }
+  
+  let value = toCelsius(77);
+  console.log(value)
